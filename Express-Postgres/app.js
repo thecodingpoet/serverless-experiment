9237fd59a4522633566973f346cbe66c3622d007
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-06-10 07:04:36 +0100

    Push expess-postgress app

--- Express-Postgres/app.js
+++ Express-Postgres/app.js
@@ -2,16 +2,13 @@ const serverless = require('serverless-http')
 const express = require('express')
 const bodyParser = require('body-parser')
 const User = require('./src/models/user')
+
 const app = express()
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.json({message: 'A simple user bio databaase'})
-})
-
-app.get('/users', (req, res) => {
-  res.json({data: 'List all users'})
 })
 
 app.post('/users', async (req, res) => {
@@ -21,7 +18,7 @@ app.post('/users', async (req, res) => {
   
   res.json({
     message: 'User saved sucessfully',
-    user: user
+    body: user
   })
 })
 
