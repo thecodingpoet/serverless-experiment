1c9319dc45fd46b7c1099288f2a6e862608002fc
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-06-10 14:56:20 +0100

    Push experiment code for hello-world app

--- Hello-World-App/handler.js
+++ Hello-World-App/handler.js
@@ -1,14 +1,23 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  const location = event.pathParameters && event.pathParameters.location
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: `Hello ${location}`
     }, null, 2),
-  };
+  }  
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.greet = async (event) => {
+  const name = event.queryStringParameters && event.queryStringParameters.name || "world!"
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello ${name}`
+    }, null, 2)
+  }
 };
